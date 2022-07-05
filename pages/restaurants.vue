31b20e16cc86ccf2b103d53ff12d54fee9b6ecf0
thirteendogs <daniel.nova.girbau@gmail.com>
2022-07-05 18:57:48 +0200

    Add select for restaurants

--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,16 +1,27 @@
 <template>
   <main class="container restaurant">
-    <h1>Restaurants</h1>
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+      <AppSelect @change="selectedRestaurant = $event" />
+      <pre>{{ $data }}</pre>
+    </div>
     <AppRestaurantInfo />
   </main>
 </template>
 
 <script>
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+import AppSelect from "@/components/AppSelect.vue";
 
 export default {
   components: {
     AppRestaurantInfo,
+    AppSelect,
+  },
+  data() {
+    return {
+      selectedRestaurant: "",
+    };
   },
 };
 </script>
