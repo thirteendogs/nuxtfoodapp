aff6554d53de98c73f0841d88a4a48b2c8de874a
thirteendogs <daniel.nova.girbau@gmail.com>
2022-07-06 19:38:36 +0200

    Add order cuantity on the cart

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -6,16 +6,21 @@
       </li>
       <li><nuxt-link exact to="/restaurants">Restaurants</nuxt-link></li>
     </ul>
+    <div class="smallnum" v-if="cartCount > 0">{{ cartCount }}</div>
     <nuxt-link to="/cart">Cart</nuxt-link>
   </nav>
 </template>
 
 <script>
 import AppLogo from "@/components/AppLogo.vue";
+import { mapGetters } from "vuex";
 
 export default {
   components: {
     AppLogo,
+  },
+  computed: {
+    ...mapGetters(["cartCount"]),
   },
 };
 </script>
